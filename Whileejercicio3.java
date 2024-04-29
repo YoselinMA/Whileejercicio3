32c556de858918ed74a494ef9a33fe38fce819ef
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-04-28 23:15:43 -0600

    Create Whileejercicio3.java

---
+++ Whileejercicio3.java
@@ -0,0 +1,15 @@
+public class Whileejercicio3 {
+    public static void main(String[] args) {
+        int contador = 0;
+
+    while (contador < 3 ){
+
+        System.out.println("Hola");
+        contador ++;
+
+    }
+
+    System.out.println("Fin");
+    }
+    
+}
